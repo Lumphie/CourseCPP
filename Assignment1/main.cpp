b1e1a8273068c0effc9aae1350972c44354550b2
Kees <wesselinkkees@gmail.com>
2016-09-29 11:17:44 +0200

    Created updateAllCells() function
    Created a function to update all cells to remove a lot of lines from main()

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <cassert>
 
 using namespace std;
+
+// returns the new state of a cell.
 
 bool updateCell(bool leftState, bool ownState, bool rightState)
 {
@@ -30,11 +33,47 @@ bool updateCell(bool leftState, bool ownState, bool rightState)
 
     // Three dimensional array with [leftNeighbor][self][rightNeihbor].
     // Array is initialized in [0][0][0], [0][0][1], [0][1][0], etc. order.
+
    bool table[2][2][2] { { {0, 1}, {1, 1} },
                          { {1, 0}, {0, 0} } };
 
    return table[leftState][ownState][rightState];
 
+}
+
+
+// Takes a pointer to an array to calculate the new states of all the cells in that array.
+void updateAllCells(bool *currentStateAutomaton, int numOfCells)
+{
+    // Create a temporary array to copy the new states from to the currentArray.
+    bool nextStateAutomaton[numOfCells];
+
+    // Loop through all cells.
+    for (int index = 0; index < numOfCells; ++index)
+    {
+    // Left neighbor of cell 0 is cell the last cell.
+        if (index == 0)
+        {
+            // Put next state in the temporary array.
+            nextStateAutomaton[0] = updateCell(currentStateAutomaton[numOfCells - 1], currentStateAutomaton[0], currentStateAutomaton[1]);
+        }
+        // Right neighbor of the last cell is cell 0.
+        else if (index == numOfCells -1)
+        {
+            // Put next state in the temporary array.
+            nextStateAutomaton[numOfCells - 1] = updateCell(currentStateAutomaton[numOfCells - 2], currentStateAutomaton[numOfCells - 1], currentStateAutomaton[0]);
+        }
+        else
+        {
+            // Put next state in a temporary array.
+            nextStateAutomaton[index] = updateCell(currentStateAutomaton[index - 1], currentStateAutomaton[index], currentStateAutomaton[index + 1]);
+        }
+    }
+
+
+    // Use temporary array to update state of all cells at the same time.
+    for (int index = 0; index < numOfCells; ++index)
+        currentStateAutomaton[index] = nextStateAutomaton[index];
 }
 
 int main()
@@ -43,47 +82,24 @@ int main()
     std::ofstream myOutputFileStream("data.csv");
 
     // Initialize the number of cells.
-    int numOfCells = 100;
+    const int numOfCells = 100;
 
-    // Create 2 arrays, one with the current state of all the cells and 1 temporary to copy the next state from.
+    // Create an array with the current state of all the cells.
     // First cell gets value 1 the rest gets value 0.
     bool currentStateAutomaton[numOfCells] {1};
-    bool nextStateAutomaton[numOfCells];
+
 
     // Loop a number of times through the process.
     for (int index = 0; index < ((3*numOfCells)/2); ++index)
     {
-        // Loop through all cells.
-        for (int index = 0; index < numOfCells; ++index)
-        {
-        // Left neighbor of cell 0 is cell the last cell.
-            if (index == 0)
-            {
-                // Put next state in the temporary array.
-                nextStateAutomaton[0] = updateCell(currentStateAutomaton[numOfCells - 1], currentStateAutomaton[0], currentStateAutomaton[1]);
-            }
-            // Right neighbor of the last cell is cell 0.
-            else if (index == numOfCells -1)
-            {
-                // Put next state in the temporary array.
-                nextStateAutomaton[numOfCells - 1] = updateCell(currentStateAutomaton[numOfCells - 2], currentStateAutomaton[numOfCells - 1], currentStateAutomaton[0]);
-            }
-            else
-            {
-                // Put next state in a temporary array.
-                nextStateAutomaton[index] = updateCell(currentStateAutomaton[index - 1], currentStateAutomaton[index], currentStateAutomaton[index + 1]);
-            }
-        }
-
-
-        // Use temporary array to update state of all cells at the same time.
-        for (int index = 0; index < numOfCells; ++index)
-            currentStateAutomaton[index] = nextStateAutomaton[index];
+        updateAllCells(currentStateAutomaton, numOfCells);
 
 
         // Print current array and output it to a .csv file.
         if (index > numOfCells / 2)
         {
+            cout << "Statenumber: " << index << ".\n";
+
             for (int index = 0; index < numOfCells; ++index)
             {
                 cout << currentStateAutomaton[index];
@@ -91,7 +107,9 @@ int main()
                 myOutputFileStream << currentStateAutomaton[index] << ' ';
             }
 
+
             myOutputFileStream << '\n';
+            cout << '\n';
 
         }
     }
